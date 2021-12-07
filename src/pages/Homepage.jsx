8b06d4630d68e114c40566ae8c1688e8056b11a1
giorgimamardashvili <eanjaparidze75@gmail.com>
2021-12-08 01:31:44 +0400

    lol

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import axios from "axios";
-import { vars } from "../vars/variables.jsx";
+import { vars } from "../variables.jsx";
+import { Imbd } from "../images/Imbd.jsx";
 
 const Homepage = () => {
   const [movies, setMovies] = React.useState([]);
   const getMovies = async () => {
     try {
-      const response = await axios(vars.MoviesDataUrl);
-      console.log(response);
+      const response = await axios.get(vars.MoviesDataUrl);
+      console.log(response.data.data);
       setMovies(response.data.data);
     } catch (e) {
       console.log(e);
@@ -16,7 +17,40 @@ const Homepage = () => {
   React.useEffect(() => {
     getMovies();
   }, []);
-  return <section></section>;
+  return (
+    <section className="movies container">
+      {movies &&
+        movies.map((movie) => (
+          <div className="movies__card" key={movie.id}>
+            <div className="movies__img">
+              <img src={Object.values(movie.covers.data)[4]} alt="movie" />
+              <div className="play">
+                <a href={movie.imdbUrl}>
+                  <img src="../images/play.png" alt="" />
+                </a>
+              </div>
+              <div className="actions">
+                <div className="langs">
+                  <span></span>
+                </div>
+              </div>
+              <div className="rates">
+                <div className="imbd">
+                  <Imbd />
+                  <span>{movie.rating.imdb.score}</span>
+                </div>
+                <div className="year">{movie.year}წ</div>
+              </div>
+            </div>
+            <div className="movies__text">
+              <a href={movie.imdbUrl}>
+                <p>{movie.originalName}</p>
+              </a>
+            </div>
+          </div>
+        ))}
+    </section>
+  );
 };
 
 export default Homepage;
