337c6799a2a279f907472ae11e2fb0d6c6c3ee42
giorgimamardashvili <eanjaparidze75@gmail.com>
2021-12-14 22:45:42 +0400

    bolo

--- src/variables.jsx
+++ src/variables.jsx
@@ -1,5 +1,6 @@
 export const vars = {
-  MoviesDataUrl: "https://api.imovies.cc/api/v1/trailers/trailer-day",
+  MoviesDataUrl:
+    "https://api.imovies.cc/api/v1/trailers/trailer-day?page=1&per_page=20&fbclid=IwAR0EaSIagG9DdaQY7uhuxIstGIleXKjZpFdhJeHyc_XMZ3Nh37DGSW8yFZA",
   SearchDataUrl: "https://movie.ge/Api/WebService/search/",
   WeatherApi: "https://api.openweathermap.org/data/2.5/weather?q=",
   ImdbUrl: "https://www.imdb.com/title",
